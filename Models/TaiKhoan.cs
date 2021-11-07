865f4247bce36a700499dd32a815d1696d0f9a38
The Tuan Phan <thetuanphan@gmail.com>
2021-11-07 15:19:46 +0700

    Init_2

---
+++ Models/TaiKhoan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn_CuaHangLaptop.Models
+{
+    public class TaiKhoan
+    {
+        string tenDangNhap;
+        string matKhau;
+
+        public TaiKhoan(string tenDangNhap, string matKhau)
+        {
+            this.tenDangNhap = tenDangNhap;
+            this.matKhau = matKhau;
+        }
+
+        public TaiKhoan()
+        {
+        }
+
+        [Key]
+        [Display(Name = "Tên đăng nhập")]
+        [StringLength(10, ErrorMessage = "Tên đăng nhập phải dưới 10 ký tự")]
+        public string TenDangNhap { get => tenDangNhap; set => tenDangNhap = value; }
+        [Required]
+        [Display(Name = "Mật khẩu")]
+        [StringLength(20, ErrorMessage = "Mật khẩu phải dưới 20 ký tự")]
+        public string MatKhau { get => matKhau; set => matKhau = value; }
+    }
+}
